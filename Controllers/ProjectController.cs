ebd48e7943315820bb6355c34105e9155769f219
Thien Nguyen Hoan <thien.nguyen.hoan@preciofishbone.se>
2020-08-25 11:11:08 +0700

    update

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace backend.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProjectController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Projects.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IFormFile files)
+        {
+            if (files != null)
+            {
+                if (files.Length > 0)
+                {
+                    var fileName = Path.GetFileName(files.FileName);
+                    var fileExtension = Path.GetExtension(fileName);
+
+                    var objfiles = new Project()
+                    {
+                        FileName = files.FileName,
+                        CreatedAt = DateTime.Now,
+                        CreatedBy = "THN",
+                        ModifiedAt = DateTime.Now,
+                        ModifiedBy = "THN"
+                    };
+
+                    using (var target = new MemoryStream())
+                    {
+                        files.CopyTo(target);
+                        objfiles.DataFiles = target.ToArray();
+                    }
+
+                    _context.Projects.Add(objfiles);
+                    _context.SaveChanges();
+
+                }
+            }
+            return RedirectToAction("Index", await _context.Projects.ToListAsync());
+        }
+
+
+        [Route("projects/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return View(project);
+        }
+
+        [Route("projects/{id}")]
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Projects
+                .FirstOrDefaultAsync(m => m.Id == id);
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return _context.Projects.Any(e => e.Id == id);
+        }
+    }
+}
